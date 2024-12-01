f14ef8b3d56083a355160d4b64d81c75189dc40d
anakarlatacon <128551833+anakarlatacon@users.noreply.github.com>
2024-12-01 16:45:29 -0300

    Update otimizarmega.js

--- otimizarmega.js
+++ otimizarmega.js
@@ -2,10 +2,10 @@ function gerarAleatorios(quantidade) {
     // Criar um array com todos os números possíveis
     let numeros = Array.from({ length: 60 }, (_, i) => i + 1);
 
-    // Embaralhar o array de números usando o algoritmo de Fisher-Yates
+    // Embaralhar o array de números 
     for (let i = numeros.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [numeros[i], numeros[j]] = [numeros[j], numeros[i]]; // Troca de posição
+        [numeros[i], numeros[j]] = [numeros[j], numeros[i]]; 
     }
 
     // Retorna os primeiros 'quantidade' números do array embaralhado
